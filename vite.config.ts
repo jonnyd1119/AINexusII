7a2f9c2d4401875822311552a2e803222735832e
jonnyd1119 <jond@graparlimited.com>
2025-10-30 15:54:11 -0400

    Update vite.config.ts- updating to just name repo

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
-  base: "/AINexusII/",  // <-- Add this line ONLY for GitHub Pages
+  base: "/AINexusII/",  // Your repo name
   plugins: [
     react(),
     runtimeErrorOverlay(),
